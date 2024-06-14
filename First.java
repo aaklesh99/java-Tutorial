6fff0fe4ce48a0f76e2c8e0d5d7a889a52850543
Aklesh kumar <90383682+aaklesh99@users.noreply.github.com>
2024-06-14 19:29:22 +0530

    First Commit

---
+++ First.java
@@ -0,0 +1,10 @@
+/**
+ * First
+ */
+public class First {
+
+    public static void main(String args[])
+    {
+        System.out.println("First Program");
+    }
+}
---
+++ Sub.java
@@ -0,0 +1,10 @@
+/**
+ * Sub
+ */
+public class Sub {
+
+     public static void main(String args[])
+     {
+        System.out.println(50-20);
+     }
+}
